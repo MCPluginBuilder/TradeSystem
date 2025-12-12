0b05ede575cd5aa711f6d312e3c40e36ee38c8cf
Alexander Mandera <alexander@mandera.eu>
2025-12-12 20:35:30 +0100

    Add 1.21.11 compatibility and update version to Hotfix 6

--- TradeSystem-Spigot/src/main/java/de/codingair/tradesystem/spigot/utils/EntityItemUtils.java
+++ TradeSystem-Spigot/src/main/java/de/codingair/tradesystem/spigot/utils/EntityItemUtils.java
@@ -2,13 +2,14 @@ package de.codingair.tradesystem.spigot.utils;
 
 import de.codingair.codingapi.server.reflections.IReflection;
 import de.codingair.codingapi.server.reflections.PacketUtils;
+import de.codingair.codingapi.server.specification.Version;
 import org.bukkit.Location;
 import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 public class EntityItemUtils {
-    private static final Class<?> ENTITY_ITEM_CLASS = IReflection.getClass(IReflection.ServerPacket.MINECRAFT_PACKAGE("net.minecraft.world.entity.item"), "EntityItem");
+    private static final Class<?> ENTITY_ITEM_CLASS = IReflection.getClass(IReflection.ServerPacket.MINECRAFT_PACKAGE("net.minecraft.world.entity.item"), Version.choose("EntityItem", 21.11, "ItemEntity"));
     private static final IReflection.ConstructorAccessor ENTITY_ITEM = IReflection.getConstructor(ENTITY_ITEM_CLASS, PacketUtils.WorldClass, double.class, double.class, double.class, PacketUtils.ItemStackClass);
 
     @NotNull
