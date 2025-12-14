b35ebbb06a279b5e4ab7fc2df329edd090f1723c
Alexander Mandera <alexander@mandera.eu>
2025-12-14 13:05:37 +0100

    Fix sounds in 1.21.4+ and fix GriefDefender dependency issue

---
+++ TradeSystem-Spigot/src/main/java/de/codingair/tradesystem/spigot/extras/external/griefdefender/ShowGriefDefenderIcon.java
@@ -0,0 +1,17 @@
+package de.codingair.tradesystem.spigot.extras.external.griefdefender;
+
+import de.codingair.tradesystem.spigot.trade.gui.layout.types.TradeIcon;
+import de.codingair.tradesystem.spigot.trade.gui.layout.types.impl.economy.ShowEconomyIcon;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+public class ShowGriefDefenderIcon extends ShowEconomyIcon {
+    public ShowGriefDefenderIcon(@NotNull ItemStack itemStack) {
+        super(itemStack, "ClaimBlocks");
+    }
+
+    @Override
+    public @NotNull Class<? extends TradeIcon> getOriginClass() {
+        return GriefDefenderIcon.class;
+    }
+}
